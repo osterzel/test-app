0033862ec09336b822239e16dce78c60a6816cfb
Oliver Sterzel <oliver@cloudsurge.co.uk>
2016-04-14 19:21:26 +0100

    Added an environment variable

--- web.py
+++ web.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+value = os.getenv('VALUE', 'Welcome to the world')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return value 
 
 if __name__ == '__main__':
     app.run()
