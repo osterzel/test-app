f9ba6284b34616f59c8b4099b0fe7a9cf69aee6a
Oliver Sterzel <oliver@cloudsurge.co.uk>
2016-04-14 20:46:43 +0100

    Updated

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 value = os.getenv('VALUE', 'Welcome to the world')
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return value 
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
